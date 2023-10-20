a7293ea8afd8acadebe5b40186336832d5fc03e5
Antonio Fernández Porrúa <antonio@hacknlove.org>
2023-10-20 12:39:48 +0200

    feat: adds solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import solidJs from "@astrojs/solid-js";
+
+export default defineConfig({
+  integrations: [solidJs()],
+});
