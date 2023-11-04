897b0de82804a78ec459b337eaf7c2f6c561f081
Antonio Fernández Porrúa <antonio@hacknlove.org>
2023-11-04 12:22:10 +0100

    waiting component

---
+++ src/components/game/waiting/Waiting.jsx
@@ -0,0 +1,37 @@
+import { websocket } from "@/js/connectWS";
+import { createSignal, onCleanup, onMount } from "solid-js";
+import "./Waiting.scss";
+
+export function Waiting() {
+  const [waiting, setWaiting] = createSignal(true);
+
+  websocket.addEventListener("message", (event) => {
+    const data = JSON.parse(event.data);
+    onMessage(data);
+  });
+
+  function onMessage(data) {
+    console.log(data);
+    switch (data.status) {
+      case "waiting":
+        setWaiting(true);
+        break;
+      case "playing":
+        setWaiting(false);
+        break;
+      default:
+        setWaiting(true);
+    }
+  }
+  onMount(() => {
+    if (websocket.lastEventData) {
+      onMessage(websocket.lastEventData);
+    }
+  });
+
+  websocket.addEventListener("close", () => {
+    setWaiting(true);
+  });
+
+  return <>{waiting() && <div id="waiting">Waiting</div>}</>;
+}
