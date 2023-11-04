f3e6ddf304e77a3d1ebf2fb25b716b76e57533fd
Antonio Fernández Porrúa <antonio@hacknlove.org>
2023-11-04 12:23:55 +0100

    deal with turns locally

---
+++ src/js/clientAlarm.js
@@ -0,0 +1,31 @@
+import { websocket } from "@/js/connectWS";
+
+let nextUpdateTimeout;
+
+websocket.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data);
+
+  if (!data.nextAt) {
+    return;
+  }
+
+  const nextAtms = data.nextAt - Date.now();
+
+  if (nextAtms <= 0) {
+    return;
+  }
+
+  if (nextUpdateTimeout) {
+    clearTimeout(nextUpdateTimeout);
+  }
+
+  nextUpdateTimeout = setTimeout(() => {
+    websocket.send(
+      JSON.stringify({
+        type: "clientSideAlarm",
+        status: data.status,
+        nextAt: data.nextAt,
+      }),
+    );
+  }, nextAtms);
+});
