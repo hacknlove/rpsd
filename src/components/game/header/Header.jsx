7f24b62ed2cc4f9b3b3710166cb0fb100b2175d3
Antonio Fernández Porrúa <antonio@hacknlove.org>
2023-11-04 12:21:54 +0100

    Header show the time for turns

--- src/components/game/header/Header.jsx
+++ src/components/game/header/Header.jsx
@@ -5,11 +5,14 @@ import "./Header.scss";
 export function Header() {
   const [timeToNext, setTimeToNext] = createSignal(0);
   const [numberOfPlayers, setNumberOfPlayers] = createSignal(0);
+  const [labelForNext, setLabelForNext] = createSignal("");
 
   websocket.addEventListener("message", (event) => {
     const data = JSON.parse(event.data);
-    console.debug(data);
+    onMessage(data);
+  });
 
+  function onMessage(data) {
     if (data.nextAt) {
       setTimeToNext(data.nextAt - Date.now());
     }
@@ -17,12 +20,22 @@ export function Header() {
     if (data.totalPlayers) {
       setNumberOfPlayers(data.totalPlayers);
     }
-  });
+
+    switch (data.status) {
+      case "waiting":
+        setLabelForNext("Time to start");
+        break;
+      case "playing":
+        setLabelForNext("Time to next");
+        break;
+      case "ended":
+        setLabelForNext("");
+    }
+  }
 
   onMount(() => {
     if (websocket.lastEventData) {
-      setTimeToNext(websocket.lastEventData.nextAt - Date.now());
-      setNumberOfPlayers(websocket.lastEventData.totalPlayers);
+      onMessage(websocket.lastEventData);
     }
   });
 
@@ -50,20 +63,25 @@ export function Header() {
   function formatedTimeToNext() {
     const miliseconds = timeToNext();
 
-    return (
+    const text =
       processInterval(miliseconds, 1000 * 60 * 60 * 24, "day", "days") ||
       processInterval(miliseconds, 1000 * 60 * 60, "hour", "hours") ||
       processInterval(miliseconds, 1000 * 60, "minute", "minutes") ||
-      processInterval(miliseconds, 1000, "second", "seconds")
-    );
+      processInterval(miliseconds, 1000, "second", "seconds") ||
+      "0 seconds";
+
+    document.title = text;
+    return text;
   }
 
   return (
     <header id="Header">
-      <div class="stats" id="timeToNext">
-        <span>Time to start:</span>
-        <span>{formatedTimeToNext()} </span>
-      </div>
+      {labelForNext() && (
+        <div class="stats" id="timeToNext">
+          <span>{labelForNext()}: </span>
+          <span>{formatedTimeToNext()} </span>
+        </div>
+      )}
       <div class="stats" id="numberOfPlayers">
         <span>Number of players:</span>
         <span>{numberOfPlayers()}</span>
